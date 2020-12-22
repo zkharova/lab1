eacf1ffd01c0468e1065c50d6c082372634fae39
zkharova <71323612+zkharova@users.noreply.github.com>
2020-12-23 00:37:22 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import csv
+import re
+
+all_symb = 0
+spaces = 0
+punct = 0
+sentences = 0
+words = 0
+
+
+f = open("steam_description_data.csv", encoding="utf-8")
+csv_reader = csv.reader(f)
+for line in csv_reader:
+            symb = ",".join(line)
+            all_symb += len(symb)
+            spaces += symb.count(" ")
+            punct += symb.count(".") + symb.count(",") + symb.count("?") \
+                + symb.count("(") + symb.count(")") + symb.count(";") \
+                + symb.count("-") + symb.count("_") + symb.count(":") \
+                + symb.count("/") + symb.count("\'") + symb.count("\"")
+            words += len(re.findall(r"(\w+'\w+)|(\w+-\w+'\w+)|(\w+-\w+'\w)|\w+", symb))
+            sentences += len(re.findall(r"([A-Z][^\.!?]*[\.!?])", symb))
+print("Число символов:", all_symb)
+print("Число символов без пробелов:", all_symb - spaces)
+print("Количество символов без знаков препинания:", all_symb - punct)
+print("Количество слов:", words)
+print("Количество предложений:", sentences)
+f.close()
