a353c95e747284c0bd7b6149901bc918df13e948
zkharova <71323612+zkharova@users.noreply.github.com>
2020-12-26 14:30:55 +0300

    Add files via upload
    + количество символов в заголовках первого уровня

--- main.py
+++ main.py
@@ -6,23 +6,22 @@ spaces = 0
 punct = 0
 sentences = 0
 words = 0
-
+header_symb = []
 
 f = open("steam_description_data.csv", encoding="utf-8")
 csv_reader = csv.reader(f)
 for line in csv_reader:
-            symb = ",".join(line)
-            all_symb += len(symb)
-            spaces += symb.count(" ")
-            punct += symb.count(".") + symb.count(",") + symb.count("?") \
-                + symb.count("(") + symb.count(")") + symb.count(";") \
-                + symb.count("-") + symb.count("_") + symb.count(":") \
-                + symb.count("/") + symb.count("\'") + symb.count("\"")
-            words += len(re.findall(r"(\w+'\w+)|(\w+-\w+'\w+)|(\w+-\w+'\w)|\w+", symb))
-            sentences += len(re.findall(r"([A-Z][^\.!?]*[\.!?])", symb))
+    symb = ",".join(line)
+    all_symb += len(symb)
+    spaces += symb.count(" ")
+    punct += symb.count(".") + symb.count(",") + symb.count("?") + symb.count("(") + symb.count(")") + symb.count(";") + symb.count("-") + symb.count("_") + symb.count(":") + symb.count("/") + symb.count("\'") + symb.count("\"")
+    words += len(re.findall(r"(\w+'\w+)|(\w+-\w+'\w+)|(\w+-\w+'\w)|\w+", symb))
+    sentences += len(re.findall(r"([A-Z][^\.!?]*[\.!?])", symb))
+    header_symb += re.findall(r'<h1>(.*?)</h1>', str(line))
 print("Число символов:", all_symb)
 print("Число символов без пробелов:", all_symb - spaces)
 print("Количество символов без знаков препинания:", all_symb - punct)
 print("Количество слов:", words)
 print("Количество предложений:", sentences)
+print("Количество символов в заголовках первого уровня:",len(str(header_symb)))
 f.close()
