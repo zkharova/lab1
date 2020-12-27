592cb1d164e4a55e4c187eb6d73db8d21da1a92e
zkharova <71323612+zkharova@users.noreply.github.com>
2020-12-27 12:43:54 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ sentences = 0
 words = 0
 header_symb = []
 
-f = open("steam_description_data.csv", encoding="utf-8")
+f = open("steam_description_data.csv", encoding="latin-1")
 csv_reader = csv.reader(f)
 for line in csv_reader:
     symb = ",".join(line)
@@ -18,10 +18,11 @@ for line in csv_reader:
     words += len(re.findall(r"(\w+'\w+)|(\w+-\w+'\w+)|(\w+-\w+'\w)|\w+", symb))
     sentences += len(re.findall(r"([A-Z][^\.!?]*[\.!?])", symb))
     header_symb += re.findall(r'<h1>(.*?)</h1>', str(line))
+head = len(' '.join(header_symb)) - (len(header_symb) - 1)
 print("Число символов:", all_symb)
 print("Число символов без пробелов:", all_symb - spaces)
 print("Количество символов без знаков препинания:", all_symb - punct)
 print("Количество слов:", words)
 print("Количество предложений:", sentences)
-print("Количество символов в заголовках первого уровня:",len(str(header_symb)))
+print("Количество символов в заголовках первого уровня:",head)
 f.close()
